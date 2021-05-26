00ec6a549d8e13071d525666bbd09f507e20d47b
Alan-Xia <237490540@qq.com>
2021-05-26 23:47:38 +0800

    完善商品列表，新增商品详情

--- pages/goods_detail/index.js
+++ pages/goods_detail/index.js
@@ -1,66 +1,31 @@
-// pages/goods_detail/index.js
+import request from '../../request/index'
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    // 页面数据
+    detail: {},
+    // 是否收藏
+    isCollect: false
   },
-
+  goods_id: "",
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    this.goods_id = options.goods_id
+    this.getShopDetail()
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+  getShopDetail () {
+    request({
+      url: "/goods/detail?goods_id=" + this.goods_id
+    }).then(res => {
+      this.setData({
+        detail: res.message
+      })
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  }
+  handleCollect (e) {}
 })
--- pages/goods_list/index.js
+++ pages/goods_list/index.js
@@ -32,7 +32,7 @@ Page({
     pagesize: 10,
   },
   // 总页数
-  total: 0,
+  total: 1,
   onLoad: function (options) {
     // 获取分类id
     this.queryParams.cid = options.cid
@@ -44,11 +44,12 @@ Page({
       url: "/goods/search",
       data: this.queryParams
     }).then(res => {
-      // console.log(res)
-      this.total = Math.ceil(res.total / this.queryParams.pagesize)
+      this.total = Math.ceil(res.message.total / this.queryParams.pagesize)
+      // console.log(this.total)
       this.setData({
         goodsList: [...this.data.goodsList,...res.message.goods]
       })
+      wx.stopPullDownRefresh()
     })
   },
   tabsItemChange (e) {
@@ -60,15 +61,33 @@ Page({
     this.setData({
       tabs
     })
+    if (index != 0) {
+      this.setData({
+        goodsList: []
+      })
+      this.getShopList()
+    }
   },
 
   // 页面相关事件处理函数--监听用户下拉动作
   onPullDownRefresh: function () {
-
+    this.setData({
+      goodsList: []
+    })
+    this.queryParams.pagenum = 1
+    this.getShopList()
   },
 
   // 页面上拉触底事件的处理函数
   onReachBottom: function () {
-
+    if (this.queryParams.pagenum >= this.total) {
+      wx.showToast({
+        title: '没有下一页数据',
+      })
+    } else {
+      this.queryParams.pagenum++
+      console.log(this.queryParams.pagenum)
+      this.getShopList()
+    }
   },
 })
